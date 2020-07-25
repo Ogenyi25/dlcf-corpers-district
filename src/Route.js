6302cb806a52c1fa5a01aad818f5bd4d1901bfe7
Ogenyi25 <johnogenyi4real@gmail.com>
2020-07-25 22:18:35 +0100

    created the boiler plate, installed sass and react-router-dom and created the routes

---
+++ src/Route.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from './screens/Home';
+
+
+
+
+class AppRouter extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                    <Switch>
+                        <Route path="/" component={Home} />
+                    </Switch>
+            </BrowserRouter>
+        );
+    }
+}
+
+export default AppRouter;
