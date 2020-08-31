a1eda04329d62107d50542608843e9271c2caacf
Ogenyi25 <johnogenyi4real@gmail.com>
2020-08-31 23:23:53 +0100

    made changes to the sunday file in the sermon componentand added styles for mobile responsiveness

--- src/sermons/Sunday.jsx
+++ src/sermons/Sunday.jsx
@@ -26,6 +26,48 @@ const SundayMessage = () => {
                     <div className="time"><i className="fa fa-clock-o" aria-hidden="true" />Time: 8:00am </div>
                 </div>
             </div>
+
+                <div className="house row container-fluid">
+                    <div className="sect col-md-3">
+                        <img className="card-img-top" src={require("../asset/images/adia.jpg")} alt="" />
+                        <h6 className="lead">Topic: This is the topic of the message.</h6>
+                        <h6 className="preacher"><i className="fa fa-user" aria-hidden="true" />Minister: Name of the preacher.</h6>
+                        <h6 className="catergory"><i className="fa fa-tags" aria-hidden="true" />Category: Message category.</h6>
+                        <div className="card-body">
+                            <audio controls className="audio" src={require("../asset/audios/adia.mp3")}></audio>
+                        </div>
+                    </div>
+
+                    <div className="sect col-md-3">
+                        <img className="card-img-top" src={require("../asset/images/adia.jpg")} alt="" />
+                        <h6 className="lead">Topic: This is the topic of the message.</h6>
+                        <h6 className="preacher"><i className="fa fa-user" aria-hidden="true" />Minister: Name of the preacher.</h6>
+                        <h6 className="catergory"><i className="fa fa-tags" aria-hidden="true" />Category: Message category.</h6>
+                        <div className="card-body">
+                            <audio controls className="audio" src={require("../asset/audios/adia.mp3")}></audio>
+                        </div>
+                    </div>
+
+                    <div className="sect col-md-3">
+                        <img className="card-img-top" src={require("../asset/images/adia.jpg")} alt="" />
+                        <h6 className="lead">Topic: This is the topic of the message.</h6>
+                        <h6 className="preacher"><i className="fa fa-user" aria-hidden="true" />Minister: Name of the preacher.</h6>
+                        <h6 className="catergory"><i className="fa fa-tags" aria-hidden="true" />Category: Message category.</h6>
+                        <div className="card-body">
+                            <audio controls className="audio" src={require("../asset/audios/adia.mp3")}></audio>
+                        </div>
+                    </div>
+
+                    <div className="sect col-md-3">
+                        <img className="card-img-top" src={require("../asset/images/adia.jpg")} alt="" />
+                        <h6 className="lead">Topic: This is the topic of the message.</h6>
+                        <h6 className="preacher"><i className="fa fa-user" aria-hidden="true" />Minister: Name of the preacher.</h6>
+                        <h6 className="catergory"><i className="fa fa-tags" aria-hidden="true" />Category: Message category.</h6>
+                        <div className="card-body">
+                            <audio controls className="audio" src={require("../asset/audios/adia.mp3")}></audio>
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 }
