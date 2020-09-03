830f17888007d347240a594924c524ea11007356
Ogenyi25 <johnogenyi4real@gmail.com>
2020-09-03 15:15:06 +0100

    added contents to the events file for the sermon components and styles for mobile responsiveness

--- src/screens/Events.jsx
+++ src/screens/Events.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../component/nav/Navbar';
+import Footer from '../component/Footer';
 
 
 const Events =()=> {
@@ -7,8 +8,127 @@ const Events =()=> {
         <div className="events-page">
             <Navbar />
             <div className="events-section">
-                
+                <div className="welcome-bg"></div>
+                <div className="title">up-coming events</div>
+                <div className="row container-fluid events-sect">
+                    <div className="col-md-3 image-sect">
+                        <div className="image">
+                            <img src={require('../asset/images/23.jpg')} alt="" className="event-img"/>
+                        </div>
+                    </div>
+                    <div className="col-md-5 text-sect">
+                        <div className="program-title">This is the title of the program</div>
+                        <div className="program-tags">
+                            <p className="date"><i className="fa fa-calendar" aria-hidden="true" />Sunday 1st, March 2020</p>
+                            <p className="date"><i className="fa fa-clock-o" aria-hidden="true" />8:00am prompt</p>
+                            <p className="date"><i className="fa fa-map-marker" aria-hidden="true" />Victory Kiddies</p>
+                        </div>
+                        <div className="program-details">
+                            Join us for a special time-out with God hikheifeildklfn ifjsdlf jfheufheilfjl
+                            jhfeufhuefnfk fdofksfk;sdfmdkjfbsdkfnlsdkjfoef
+                            hfygfseijfn emjbjhfaoaijfei fjef
+                            jndiihuihfsljfn ejfe hfodk;ofjiofjeijfieuh fuejfiejiojeofjieh feihfiolsf
+                            jdfjhfu jf oejei huiehuehieufnkljsskd;lkfpodkokdoedeokejf jdfjoljflejf
+                        </div>
+                    </div>
+                    <div className="col-md-2 btn-sect">
+                        <div>
+                        <button className="btn">Read More</button>
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div className="row container-fluid events-sect">
+                    <div className="col-md-3 image-sect">
+                        <div className="image">
+                            <img src={require('../asset/images/23.jpg')} alt="" className="event-img"/>
+                        </div>
+                    </div>
+                    <div className="col-md-5 text-sect">
+                        <div className="program-title">This is the title of the program</div>
+                        <div className="program-tags">
+                            <p className="date"><i className="fa fa-calendar" aria-hidden="true" />Sunday 1st, March 2020</p>
+                            <p className="date"><i className="fa fa-clock-o" aria-hidden="true" />8:00am prompt</p>
+                            <p className="date"><i className="fa fa-map-marker" aria-hidden="true" />Victory Kiddies</p>
+                        </div>
+                        <div className="program-details">
+                            Join us for a special time-out with God hikheifeildklfn ifjsdlf jfheufheilfjl
+                            jhfeufhuefnfk fdofksfk;sdfmdkjfbsdkfnlsdkjfoef
+                            hfygfseijfn emjbjhfaoaijfei fjef
+                            jndiihuihfsljfn ejfe hfodk;ofjiofjeijfieuh fuejfiejiojeofjieh feihfiolsf
+                            jdfjhfu jf oejei huiehuehieufnkljsskd;lkfpodkokdoedeokejf jdfjoljflejf
+                        </div>
+                    </div>
+                    <div className="col-md-2 btn-sect">
+                        <div>
+                        <button className="btn">Read More</button>
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div className="row container-fluid events-sect">
+                    <div className="col-md-3 image-sect">
+                        <div className="image">
+                            <img src={require('../asset/images/23.jpg')} alt="" className="event-img"/>
+                        </div>
+                    </div>
+                    <div className="col-md-5 text-sect">
+                        <div className="program-title">This is the title of the program</div>
+                        <div className="program-tags">
+                            <p className="date"><i className="fa fa-calendar" aria-hidden="true" />Sunday 1st, March 2020</p>
+                            <p className="date"><i className="fa fa-clock-o" aria-hidden="true" />8:00am prompt</p>
+                            <p className="date"><i className="fa fa-map-marker" aria-hidden="true" />Victory Kiddies</p>
+                        </div>
+                        <div className="program-details">
+                            Join us for a special time-out with God hikheifeildklfn ifjsdlf jfheufheilfjl
+                            jhfeufhuefnfk fdofksfk;sdfmdkjfbsdkfnlsdkjfoef
+                            hfygfseijfn emjbjhfaoaijfei fjef
+                            jndiihuihfsljfn ejfe hfodk;ofjiofjeijfieuh fuejfiejiojeofjieh feihfiolsf
+                            jdfjhfu jf oejei huiehuehieufnkljsskd;lkfpodkokdoedeokejf jdfjoljflejf
+                        </div>
+                    </div>
+                    <div className="col-md-2 btn-sect">
+                        <div>
+                        <button className="btn">Read More</button>
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div className="row container-fluid events-sect">
+                    <div className="col-md-3 image-sect">
+                        <div className="image">
+                            <img src={require('../asset/images/23.jpg')} alt="" className="event-img"/>
+                        </div>
+                    </div>
+                    <div className="col-md-5 text-sect">
+                        <div className="program-title">This is the title of the program</div>
+                        <div className="program-tags">
+                            <p className="date"><i className="fa fa-calendar" aria-hidden="true" />Sunday 1st, March 2020</p>
+                            <p className="date"><i className="fa fa-clock-o" aria-hidden="true" />8:00am prompt</p>
+                            <p className="date"><i className="fa fa-map-marker" aria-hidden="true" />Victory Kiddies</p>
+                        </div>
+                        <div className="program-details">
+                            Join us for a special time-out with God hikheifeildklfn ifjsdlf jfheufheilfjl
+                            jhfeufhuefnfk fdofksfk;sdfmdkjfbsdkfnlsdkjfoef
+                            hfygfseijfn emjbjhfaoaijfei fjef
+                            jndiihuihfsljfn ejfe hfodk;ofjiofjeijfieuh fuejfiejiojeofjieh feihfiolsf
+                            jdfjhfu jf oejei huiehuehieufnkljsskd;lkfpodkokdoedeokejf jdfjoljflejf
+                        </div>
+                    </div>
+                    <div className="col-md-2 btn-sect">
+                        <div>
+                        <button className="btn">Read More</button>
+                        </div>
+                    </div>
+                </div>
             </div>
+            <Footer />
         </div>
     )
 }
