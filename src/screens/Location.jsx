3e426d96c6e58d90a6da6aac95b525adf777fdbb
Ogenyi25 <johnogenyi4real@gmail.com>
2020-09-07 22:09:18 +0100

    updated site contents

--- src/screens/Location.jsx
+++ src/screens/Location.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from '../component/nav/Navbar';
+import Footer from '../component/Footer';
 
 
 const Location = () => {
@@ -18,30 +19,8 @@ const Location = () => {
                         <div className="container">
                             <div className="front side">
                                 <div className="content">
-                                    <h1>Hakkam Abdullah</h1>
-                                    <p>I am a graphic designer and art director. I am as well specialised in front 
-                                        end web design, user experience and creating identities. Throughout my career, 
-                                        I have worked with companies of all shapes and sizes that enriched my experience
-			                        </p>
-                                </div>
-                            </div>
-                            <div className="back side">
-                                <div className="content">
-                                    <div className="venue"><i className="fa fa-map-marker" aria-hidden="true" />Location: This is the location of the venue</div>
-                                    <div className="contact"><i className="fa fa-map-phone" aria-hidden="true" />Contact: This is the contact of the fellowship</div>
-		                        </div>
-                            </div>
-
-                        </div>
-                    </div>
-                    <div className="location col-md-3">
-                    <div className="container">
-                            <div className="front side">
-                                <div className="content">
-                                    <h1>Hakkam Abdullah</h1>
-                                    <p>I am a graphic designer and art director. I am as well specialised in front 
-                                        end web design, user experience and creating identities. Throughout my career, 
-                                        I have worked with companies of all shapes and sizes that enriched my experience
+                                    <div>CITY COLLEGE OF EDUCATION</div>
+                                    <p>
 			                        </p>
                                 </div>
                             </div>
@@ -58,10 +37,26 @@ const Location = () => {
                     <div className="container">
                             <div className="front side">
                                 <div className="content">
-                                    <h1>Hakkam Abdullah</h1>
-                                    <p>I am a graphic designer and art director. I am as well specialised in front
-                                        end web design, user experience and creating identities. Throughout my career, 
-                                        I have worked with companies of all shapes and sizes that enriched my experience
+                                    <div>CITY COLLEGE OF EDUCATION</div>
+                                    <p>
+			                        </p>
+                                </div>
+                            </div>
+                            <div className="back side">
+                                <div className="content">
+                                    <div className="venue"><i className="fa fa-map-marker" aria-hidden="true" />Location: This is the location of the venue</div>
+                                    <div className="contact"><i className="fa fa-phone" aria-hidden="true" />Contact: This is the contact of the fellowship</div>
+		                        </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="location col-md-3">
+                    <div className="container">
+                            <div className="front side">
+                                <div className="content">
+                                    <div>CITY COLLEGE OF EDUCATION</div>
+                                    <p>
 			                        </p>
                                 </div>
                             </div>
@@ -76,6 +71,7 @@ const Location = () => {
                     </div>
                 </div>
             </div>
+            <Footer />
         </div>
     )
 }
